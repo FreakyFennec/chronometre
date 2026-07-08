45b286345046f88b8776880dd5c94cd360bae946
FreakyFennec <ahjinani@gmail.com>
2026-07-08 03:56:58 +0200

    Add chronometre folder

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = "chrono-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then(cache => cache.addAll(FILES))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
